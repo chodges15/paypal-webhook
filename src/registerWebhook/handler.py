0e6071d11cc4aa0726a876ad635fb4b0c9ab14cc
Chris Hodges <chris.hodges@gmail.com>
2020-06-13 00:36:28 -0500

    Update

--- src/registerWebhook/handler.py
+++ src/registerWebhook/handler.py
@@ -1,6 +1,47 @@
 import json
-def handler(event, context):
-    # Log the event argument for debugging and for use in local development.
-    print(json.dumps(event))
+import os
+import boto3
+from botocore.exceptions import ClientError
+import paypalrestsdk
 
-    return {}
+def get_secret(secret_name):
+  secrets_namespace = os.environ['SECRETS_NAMESPACE']
+  secret_id = secrets_namespace + secret_name
+  try:
+    response = client.get_secret_value(SecretId=secret_id)
+  except ClientError as e:
+    print("Error fetching secret")
+    exit(1)
+  else:
+    return response['SecretString']
+
+
+def handler(message, context):
+  secretsRegion = os.environ['SECRETS_REGION']
+
+  session = boto3.session.Session()
+  client = session.client(
+      service_name='secretsmanager',
+      region_name=secretsRegion
+  )
+
+  paypalrestsdk.configure({
+    "mode": "sandbox" if os.environ['SECRETS_NAMESPACE'] == "development" else "live",
+    "client_id": get_secret('paypal_client_id'),
+    "client_secret": get_secret('paypal_secret') 
+  })
+
+  webhook = Webhook({
+    "url": "https://www.yeowza.com/paypal_webhook",
+    "event_types": [
+        {
+            "name": "PAYMENT.AUTHORIZATION.CREATED"
+        },
+        {
+            "name": "PAYMENT.AUTHORIZATION.VOIDED"
+        }
+    ]
+  })
+
+
+  return {}
